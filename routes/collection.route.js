6e53e7be300885e5a57d1867a9d9aa8269142e89
chandan <chandansutradhar2@gmail.com>
2022-11-23 11:44:23 +0530

    route and controller setup

---
+++ routes/collection.route.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { route } = require("./users.route");
+const router = express.Router();
+
+// router.get("/all", (req, res) => {
+//   res.send("all collection list");
+// });
+
+module.exports = router;
