43ffe11f466158324fab4146abe3b985028cd7eb
chandan <chandansutradhar2@gmail.com>
2022-11-23 11:00:13 +0530

    initial endpoint defination

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require("express");
+const express = require("express"); // import {express} from 'express
 const app = express();
 const port = 3000;
+app.use(express.json()); //define app will accept request in json (body parser)
 
+//route endpoint - REST API endpoint
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/authenticate", function cb(request, response) {
+  console.log(request.body);
+  response.status(200).send("autheticate fail");
 });
 
 app.listen(port, () => {
